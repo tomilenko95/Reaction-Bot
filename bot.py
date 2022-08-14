cf207b7f1a38abcd9d9f3cde0b4d3c11b738d3b8
UnBest <54543093+tomilenko95@users.noreply.github.com>
2022-08-14 09:02:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,10 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and not filename.startswith("_"):
         bot.load_extension(f"cogs.{filename[:-3]}")
  
+
+discord.opus.load_opus()
+if not discord.opus.is_loaded():
+    raise RunTimeError('Opus failed to load')
  
 #voice.play(discord.FFmpegPCMAudio(executable="ffmpeg/bin/ffmpeg.exe", **FFMPEG_OPTIONS))
 
