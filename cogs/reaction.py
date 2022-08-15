fa2b976325acd05c15ebc845258ddc890adb77e1
Wair <creep.zip.channel@gmail.com>
2022-08-15 19:55:28 +0300

    Update reaction.py

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -97,7 +97,8 @@ class Reaction(commands.Cog):
     @commands.command()
     async def hug(self, ctx, member: discord.Member = None):
         if member is None:
-            member = ctx.author
+            embed = discord.Embed(color=member.color, title="Ошибка")
+            embed.description = f"Укажите пользователя"
         embed = discord.Embed(color=member.color, title="Обнимашки")
         embed.description = f"{ctx.author.mention} Обнял(а) {member.mention}"
         url = (random.choice(hgs))
