30aaacb55b06c900c03688b67d5a54c857f32184
UnBest <54543093+tomilenko95@users.noreply.github.com>
2022-08-15 22:32:49 +0300

    Update Mus.py

--- cogs/Mus.py
+++ cogs/Mus.py
@@ -139,21 +139,5 @@ class fucking(commands.Cog):
         await self.vc.disconnect()
 
 
-
-
-    @commands.command()
-    async def help(self, ctx, member: discord.Member = None):
-        guild = ctx.guild
-        gname = guild.name
-        clientname = config["settings"]["name"]
-        embed=discord.Embed(title= "<a:7546tree:919153727913476138>Пожалуйста выберите категорию<a:7546tree:919153727913476138>", color=2899536)
-        embed.set_thumbnail(url="https://images-ext-2.discordapp.net/external/nVKS-mrrI3YGoI-HZZkGeBAM2FbGHRedipqXkTNO4Z8/%3Fsize%3D48/https/cdn.discordapp.com/emojis/894237267252613160.gif")
-        embed.add_field(name="`#help1`", value="*<a:3496diamond:920995053491605636>Экономика*", inline=True)
-        embed.add_field(name="`#help2`", value="*<a:6325squirtlehype:920994776667553833>Развлечения*", inline=False)
-        embed.add_field(name="`#help3`", value="*<a:3335snowflake:920995642703224853>Разное*", inline=False)
-        
-        embed.set_footer(text=f"{gname} • {clientname}")
-        await ctx.send(embed=embed)
-
 def setup(bot):
     bot.add_cog(fucking(bot))
