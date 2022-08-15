f1741cc496d0c9d5263bbac50fea5e78dd31738f
Wair <creep.zip.channel@gmail.com>
2022-08-15 19:33:06 +0300

    Update reactions

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -17,6 +17,10 @@ fuk = ['https://cdn.discordapp.com/attachments/1001737508205375558/1008126551474
        'https://cdn.discordapp.com/attachments/1001737508205375558/1008130412196929607/ezgif.com-gif-maker.gif'
         ]
 
+puts = ['https://media.discordapp.net/attachments/1003002531024228352/1008363607215591434/a9c771e756e3a920c0923a109f32c145.gif',
+        'https://media.discordapp.net/attachments/1003002531024228352/1008363607538540594/b459058ae02cb62ce92c30f5be117928.gif'
+        ]
+
 
 
 class Reaction(commands.Cog):
@@ -42,12 +46,23 @@ class Reaction(commands.Cog):
 
 
     @commands.command()
-    async def fuck(self, ctx, member: discord.Member = None):
+    async def put(self, ctx, member: discord.Member = None):
         if member is None:
             member = ctx.author
-        embed = discord.Embed(color=member.color, title="FUCK")
-        embed.description = f"{ctx.author.mention} Трахнул(а) {member.mention}"
+        embed = discord.Embed(color=member.color, title="Поглаживание")
+        embed.description = f"{ctx.author.mention} Погладил(а) {member.mention}"
         url = (random.choice(fuk))
+        embed.set_image(url=url)
+        await ctx.send(embed=embed)
+
+
+    @commands.command()
+    async def hug(self, ctx, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+        embed = discord.Embed(color=member.color, title="Обнимашки")
+        embed.description = f"{ctx.author.mention} Обнял(a) {member.mention}"
+        url = (random.choice(hugs))
         embed.set_image(url=url)
         await ctx.send(embed=embed)
 
